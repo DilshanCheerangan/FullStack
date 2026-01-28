5ae887002b8b0de368dcdb29aa2690944ed9182c
DilshanCheerangan <dilshanmhd.wayanad@gmail.com>
2026-01-28 12:05:55 +0530

    all done

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -27,6 +27,9 @@ const App = () => {
     setVotes(copy)
   }
 
+  const maxVotes = Math.max(...votes)
+  const indexOfMax = votes.indexOf(maxVotes)
+
   return (
     <div>
       <h1>Anecdote of the day</h1>
@@ -34,6 +37,10 @@ const App = () => {
       <p>has {votes[selected]} votes</p>
       <button onClick={handleVote}>vote</button>
       <button onClick={handleNextAnecdote}>next anecdote</button>
+
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[indexOfMax]}</p>
+      <p>has {maxVotes} votes</p>
     </div>
   )
 }
