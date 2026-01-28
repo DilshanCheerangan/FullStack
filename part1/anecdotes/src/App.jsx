edaccaa1a07298cbf82a59d3231a4d8074e8b1bf
DilshanCheerangan <dilshanmhd.wayanad@gmail.com>
2026-01-28 12:04:39 +0530

    1.13 done

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -3,27 +3,36 @@ import { useState } from 'react'
 
 const App = () => {
   const anecdotes = [
-    'hello',
-    'my name is dilshan!',
-    'i am a software engineer.',
-    'i study at university of colombo school of computing.',
-    'i am a good man',
-    'javascript is good programming language.',
-    'the future is ai',
-    'thank you for listening'
+    'Writing code every day makes you better.',
+    'Reading other people’s code teaches you a lot.',
+    'Small commits are easier to understand and review.',
+    'Clear names make code easier to read.',
+    'Testing your code helps you find bugs early.',
+    'Taking breaks can help you solve problems faster.',
+    'Writing comments for tricky parts saves time later.',
+    'Practice is the best way to learn programming.'
   ]
 
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
 
   const handleNextAnecdote = () => {
     const randomIndex = Math.floor(Math.random() * anecdotes.length)
     setSelected(randomIndex)
   }
 
+  const handleVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
+
   return (
     <div>
       <h1>Anecdote of the day</h1>
       <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={handleVote}>vote</button>
       <button onClick={handleNextAnecdote}>next anecdote</button>
     </div>
   )
