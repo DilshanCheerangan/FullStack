0e4c6e101ce47d2b32c39808d8371a1391041d88
DilshanCheerangan <dilshanmhd.wayanad@gmail.com>
2026-02-04 09:44:25 +0530

    2.2 done

--- part2/courseinfo-part2/src/Course.jsx
+++ part2/courseinfo-part2/src/Course.jsx
@@ -1,25 +1,22 @@
+const Course = ({ course }) => {
+  const total = course.parts.reduce(
+    (sum, part) => sum + part.exercises,
+    0
+  )
 
-const Header = ({ name }) => <h1>{name}</h1>
+  return (
+    <div>
+      <h1>{course.name}</h1>
 
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-)
+      {course.parts.map(part => (
+        <p key={part.id}>
+          {part.name} {part.exercises}
+        </p>
+      ))}
 
-const Content = ({ parts }) => (
-  <div>
-    {parts.map(part => (
-      <Part key={part.id} part={part} />
-    ))}
-  </div>
-)
-
-const Course = ({ course }) => (
-  <div>
-    <Header name={course.name} />
-    <Content parts={course.parts} />
-  </div>
-)
+      <p>Total of {total} exercises</p>
+    </div>
+  )
+}
 
 export default Course
